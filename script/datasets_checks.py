2bf07564f9918a5f3d55e2c5a0082428326b920e
Johann Dreo <johann.dreo@pasteur.fr>
2026-05-29 16:16:15 +0200

    feat(checks): more checks on set differences

--- script/datasets_checks.py
+++ script/datasets_checks.py
@@ -9,7 +9,7 @@ def log_head(s, max = 15, indent = 1):
             logging.debug(f"{t}[…]")
             break
 
-def load(filename):
+def load(filename, max = 15):
     res = set()
     dups = set()
     logging.info(f"Load {filename} ...")
@@ -29,7 +29,9 @@ def load(filename):
     ndup = len(lines) - len(res)
     if ndup > 0:
         logging.debug(f"\tcontains {ndup} duplicates:")
-        log_head(dups, indent = 2)
+        log_head(dups, max, indent = 2)
+    else:
+        logging.debug(f"\tcontains no duplicate")
     return res
 
 
@@ -56,7 +58,10 @@ def tgt(s):
 def check_no_intersection(data, lhs, rhs):
     inter = data[lhs].intersection(data[rhs])
     if inter:
-        logging.error(f"{lhs} and {rhs} intersect on: {inter}")
+        logging.error(f"{lhs} and {rhs} intersect on:")
+        log_head(inter)
+    else:
+        logging.info(f"{lhs} and {rhs} do not intersect")
 
 
 def check_no_intersections(data, sets):
@@ -72,11 +77,14 @@ if __name__ == "__main__":
     parser.add_argument("-a", "--train", default="skg__train.txt")
     parser.add_argument("-v", "--valid", default="skg__validation.txt")
     parser.add_argument("-t", "--test", default="skg__test.txt")
-    parser.add_argument("-n", "--names", default="entity_names.txt")
-    parser.add_argument("-y", "--types", default="entity_types.txt")
     parser.add_argument("-b", "--brg", default="brg.txt")
+    parser.add_argument("-s", "--skg", default="skg.txt")
+    parser.add_argument("-y", "--types", default="entity_types.txt")
+    parser.add_argument("-n", "--names", default="entity_names.txt")
 
     parser.add_argument("--log-level", default = "DEBUG")
+    parser.add_argument("--max", default = 10)
+
 
     asked = parser.parse_args()
 
@@ -86,20 +94,24 @@ if __name__ == "__main__":
     logger.setLevel(level)
 
     data = {}
-    data["test"]  = load(asked.test)
-    data["train"] = load(asked.train)
-    data["valid"] = load(asked.valid)
-    data["names"] = load(asked.names)
-    data["types"] = load(asked.types)
-    data["brg"]   = load(asked.brg)
+    data["test"]  = load(asked.test, asked.max)
+    data["train"] = load(asked.train, asked.max)
+    data["valid"] = load(asked.valid, asked.max)
+    data["brg"]   = load(asked.brg, asked.max)
+    data["skg"]   = load(asked.skg, asked.max)
+    data["types"] = load(asked.types, asked.max)
+    data["names"] = load(asked.names, asked.max)
 
-    if sources(data, "names") != sources(data, "types"):
-        logging.error(f"{asked.types} and {asked.names} do not have the same elements IDs")
-        symdiff = sources(data, "names").symmetric_difference(sources(data, "types"))
-        if symdiff:
-            log_head(symdiff)
-        
-        
+    if sources(data, "names") == sources(data, "types"):
+        logging.info(f"{asked.types} and {asked.names} have the same elements IDs")
+    else:
+        logging.error(f"{asked.types} and {asked.names} DO NOT have the same elements IDs")
+
+    if data["skg"] == data["train"] | data["valid"] | data["test"]:
+        logging.info(f"{asked.skg} is the same than the union of train+valid+test")
+    else:
+        logging.error(f"{asked.skg} is NOT the same than the union of train+valid+test")
+
     check_no_intersections(data, ["train", "valid", "test"])
 
     all_names = \
@@ -109,10 +121,28 @@ if __name__ == "__main__":
         | sources(data, "brg")  | targets(data, "brg")
 
     recorded = sources(data, "names")
-    if recorded != all_names:
+    if recorded == all_names:
+        logging.info(f"{asked.names} and input SKG names (train+valid+test+brg) are the same")
+    else:
         n_recorded = len(recorded)
         n_all = len(all_names)
         symdiff = recorded.symmetric_difference(all_names)
-        logging.error(f"{n_recorded} input names (train+valid+test+brg) differ from {n_all} {asked.names} by {len(symdiff)}")
         if symdiff:
-            log_head(symdiff)
+            logging.error(f"{n_recorded} input SKG names (train+valid+test+brg) differ from {n_all} sources in {asked.names}:")
+            logging.error(f"There's {len(symdiff)} names that differs.")
+
+            recorded_diff = recorded.difference(all_names)
+            if recorded_diff:
+                logging.error(f"There's {len(recorded_diff)} names that are in {asked.names} but not in input SKG names (train+valid+test+brg)")
+                log_head(recorded_diff, asked.max)
+
+            all_diff = all_names.difference(recorded)
+            if all_diff:
+                logging.error(f"There's {len(all_diff)} names that are in input SKG names (train+valid+test+brg) but not in {asked.names}")
+                log_head(all_diff, asked.max)
+
+                for k in ["train", "valid", "test", "brg"]:
+                    diff = all_names.difference(recorded)
+                    if diff:
+                        logging.error(f"There's {len(diff)} names that are in {vars(asked)[k]} but not in {asked.names}")
+
